5be5ec70cdfedbad8d0bfa36c0de32948f81b4e2
lorentinbuca <lorentinb@hotmail.com>
2015-10-30 14:14:05 -0700

    Added images to correct answers

--- d-2-code-review.js
+++ d-2-code-review.js
@@ -1,5 +1,3 @@
-alert("Are you ready to play?");
-
 var score = 0;
 
 var ans1 = document.getElementById("answer1");
@@ -14,18 +12,18 @@ function firstQuestion() {
   if (question1 === "yes") {
   ans1.innerHTML = "Incorrect! My nationality is American.";
   } else if (question1 === "no") {
-  ans1.innerHTML = "Correct! I am not South Korean.";
+  ans1.innerHTML = "Correct! I am not South Korean." + '<br><img style="height:200px; width:200px;" src="img/tacodog.jpg">';
   score++;
   } else {
   ans1.innerHTML = "Sorry! " + question1 + " That is not one of the listed answers.";
   }
 }
-firstQuestion();
+setTimeout(firstQuestion, 1000);
 
 function secondQuestion() {
   var question2 = prompt("Is my favorite superhero, spiderman, yes or no?").toLowerCase();
   if (question2 === "yes") {
-  ans2.innerHTML = "Correct! My favorite superhero is indeed, spiderman";
+  ans2.innerHTML = "Correct! My favorite superhero is indeed, spiderman" + '<br><img style="height:200px; width:200px;" src="img/tacodog.jpg">';
   score++;
   } else if (question2 === "no") {
   ans2.innerHTML = "Incorrect! Spiderman is in fact my favorite superhero.";
@@ -33,25 +31,27 @@ function secondQuestion() {
   ans2.innerHTML = "Sorry! " + question2 + " That is not one of the listed answers.";
   }
 }
-secondQuestion();
+
+setTimeout(secondQuestion, 1050);
 
 function thirdQuestion() {
   var question3= prompt("Do I have a younger brother, yes or no?").toLowerCase();
   if (question3 === "yes") {
   ans3.innerHTML = "Incorrect! However, I do have an older brother.";
   } else if (question3 === "no") {
-  ans3.innerHTML = "Correct! I do not have a younger brother.";
+  ans3.innerHTML = "Correct! I do not have a younger brother." + '<br><img style="height:200px; width:200px;" src="img/tacodog.jpg">';
   score++
   } else {
   ans3.innerHTML = "Sorry! " + question3 + " That is not one of the listed answers.";
   }
 }
-thirdQuestion();
+
+setTimeout(thirdQuestion, 1100);
 
 function fourthQuestion() {
   var question4= prompt("Am I legally an adult in age, yes or no?");
   if (question4 === "yes") {
-  ans4.innerHTML = "Correct! I am 20 years old.";
+  ans4.innerHTML = "Correct! I am 20 years old." + '<br><img style="height:200px; width:200px;" src="img/tacodog.jpg">';
   score++
   } else if (question4 === "no") {
   ans4.innerHTML = "Incorrect! I am actually 20 years of age, two years over the legal age.";
@@ -59,33 +59,42 @@ function fourthQuestion() {
   ans4.innerHTML = "Sorry! " + question4 + " That is not one of the listed answers.";
   }
 }
-fourthQuestion();
+
+
+
+setTimeout(fourthQuestion, 1150);
 
 function fifthQuestion() {
   var question5= prompt("Am I a sports fanatic, yes or no?");
   if (question5 === "yes") {
-  ans5.innerHTML = "Incorrect! I do not watch or play a lot of sports.";
+  ans5.innerHTML = "Incorrect! I do not watch or play a lot of sports." ;
   } else if (question5 === "no") {
-  ans5.innerHTML = "Correct! I am not a sports fanatic.";
+  ans5.innerHTML = "Correct! I am not a sports fanatic." + '<br><img style="height:200px; width:200px;" src="img/tacodog.jpg">';
   score++;
   } else {
   ans5.innerHTML = "Sorry! " + question5 + " That is not one of the listed answers.";
   }
 }
-fifthQuestion();
+setTimeout(fifthQuestion, 1200);
 
 function sixthQuestion() {
   var question6= prompt("Do I like playing video games, yes or no?");
   if (question6 === "yes") {
-  ans6.innerHTML = "Correct! I absolutely love playing video games.";
+  ans6.innerHTML = "Correct! I absolutely love playing video games." + '<br><img style="height:200px; width:200px;" src="img/tacodog.jpg">';
   score++;
   } else if (question6 === "no") {
   ans6.innerHTML = "Incorrect! I love to play video games.";
   } else {
   ans6.innerHTML = "Sorry! " + question6 + " That is not one of the listed answers.";
+
   }
+  alert("You got " + score + " correct answers out of 6!");
 }
-sixthQuestion();
+
+setTimeout(sixthQuestion, 1250);
 
 
-alert("You got " + score + " correct answers out of 6!");
+
+
+
+
